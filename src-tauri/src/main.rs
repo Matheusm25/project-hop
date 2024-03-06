2717ac30452aef4cad5ebd6f843dd9904d22c0bd
Matheus Marques Lima <matheusm25@hotmail.com>
2024-03-06 20:45:03 -0300

    chore: save and open project

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,24 +1,49 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::fs;
+use std::{fs};
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use tauri::api::path::home_dir;
 
 #[tauri::command]
 fn get_sources() -> Vec<String> {
-    let paths: Vec<String> = fs::read_to_string("/home/matheus/github/project-hop/src-tauri/src/sources").unwrap().lines().map(|s| s.to_string()).collect();
-    paths
+    let user_dir = home_dir().unwrap().into_os_string();
+    
+    match fs::read_to_string(format!("{}/.hop-sources", user_dir.to_str().unwrap())) {
+        Ok(file) => file.lines().map(|s| s.to_string()).collect(),
+        Err(_error) => [].to_vec()
+    }
+}
+
+#[tauri::command]
+fn update_source(source_name: String, source_path: String) {
+    let user_dir = home_dir().unwrap().into_os_string();
+    
+
+    match fs::read_to_string(format!("{}/.hop-sources", user_dir.to_str().unwrap())) {
+        Ok(file) => {
+            fs::write(
+                format!("{}/.hop-sources", user_dir.to_str().unwrap()),
+                format!("{}\n{} {}", file, source_name, source_path)
+            ).unwrap()
+
+        },
+        Err(_error) => {
+            match fs::write(
+                format!("{}/.hop-sources", user_dir.to_str().unwrap()),
+                format!("{} {}", source_name, source_path)
+            ) {
+
+                Ok(_file) => print!("File was created"),
+                Err(error) => panic!("Problem creating the file: {:?}", error),
+            }
+        }
+    }
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
-        .invoke_handler(tauri::generate_handler![get_sources])
+        .invoke_handler(tauri::generate_handler![update_source, get_sources])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ const { invoke } = window.__TAURI__.tauri;
 const events = window.__TAURI__.event;
 const process = window.__TAURI__.process;
 const shell = window.__TAURI__.shell;
+const dialog = window.__TAURI__.dialog;
 
 let greetInputEl;
 let greetMsgEl;
@@ -9,6 +10,7 @@ let sources = {};
 
 window.addEventListener("DOMContentLoaded", async () => {
   const sourcesString = await invoke('get_sources')
+  
   for (const source of sourcesString) {
     sources[source.split(' ')[0]] = source.split(' ')[1];
   }
@@ -21,15 +23,30 @@ window.addEventListener("DOMContentLoaded", async () => {
     .addEventListener('keydown', async (e) => {
       if (e.key === 'Enter') {
         const project = document.querySelector('#project-input').value;
-        console.log('project', project, sources[project]);
         if (sources[project]) {
           const command = new shell.Command('code', [sources[project]]);
           await command.spawn();
           await sleep(1000);
           process.exit(0);
+        } else {
+          document.querySelector('#project-input').value = '';
         }
-      }
+      } 
     });
+
+    document
+      .querySelector('#project-button')
+      .addEventListener('click', async e => {
+        const path = await dialog.open({ directory: true  });
+        const project = document.querySelector('#project-input').value;
+
+        await invoke('update_source', { sourceName: project, sourcePath: path });
+
+        const command = new shell.Command('code', [path]);
+          await command.spawn();
+          await sleep(1000);
+          process.exit(0);
+      });
 });
 
 async function sleep(ms) {
